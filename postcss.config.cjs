d48057f66ad0f1b6ca386f30a6e61c6b7fd88341
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2025-05-22 22:44:20 +0100

    oa

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,10 @@
-// postcss.config.cjs
+postcss.config.cjs
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [
-    // Ensure you have these packages installed:
-    // npm install -D @tailwindcss/postcss autoprefixer
-    require('@tailwindcss/postcss'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {}, // Next.js expects the string 'tailwindcss' and will resolve it
+    autoprefixer: {}, // Next.js expects the string 'autoprefixer' and will resolve it
+  },
 };
 
 module.exports = config;
