4b8acbb8b5e342e9e739f5987e1fabb93b383687
mane <manecpedro@gmail.com>
2025-05-23 12:07:39 +0100

    final setup changes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-
-=======
   /* config options here */
->>>>>>> origin/main
 };
 
 export default nextConfig;
